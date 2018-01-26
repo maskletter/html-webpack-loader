be1cd31df376b067d9f056366b1b6572eec14bb4
13146595984 <504840815@qq.com>
2018-01-26 18:05:10 +0800

    21

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const loaderUtils = require("loader-utils");
+const tool = require('./lib/tool');
+
+const replaceHtml = tool.Replace;
+
+
+
+const _html_Style_loader = function(content){
+
+
+	
+
+	(function(options){
+		for(let i in options){
+			tool.method.options[i] = options[i];
+		}
+
+	}(loaderUtils.getOptions(this) || {}))
+
+	content = replaceHtml.assemble(content, this.context);
+	
+
+
+	return 'module.exports = "'+content+'"';
+}
+
+module.exports = _html_Style_loader;
+
+
+
+
