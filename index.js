a69e156a628c498ed68611a9c71a137485f667aa
13146595984 <504840815@qq.com>
2018-02-05 16:55:12 +0800

    版本更新，修复一箩筐bug

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 const loaderUtils = require("loader-utils");
 const tool = require('./lib/tool');
+const fs = require('fs');
 
-const replaceHtml = tool.Replace;
-
-
-
-const _html_Style_loader = function(content){
+const htmlReplace = tool.HtmlReplace;
 
 
-	
+
+const _html_webpack_loader = function(content){
+
 
 	(function(options){
 		for(let i in options){
@@ -18,15 +17,22 @@ const _html_Style_loader = function(content){
 
 	}(loaderUtils.getOptions(this) || {}))
 
-
-	content = replaceHtml.assemble(content, this.context);
+	content = htmlReplace.assemble(content, this.context);
 	
 
+	var exportsString = "module.exports = ";
+	
+	return exportsString + JSON.stringify(content).replace(/\${mask_x_x_letter#(.*?)#mask_x_x_letter}/g, (aims, str1, str2) => {
+		let data = tool.method.db.resources[aims];
 
-	return 'module.exports = "'+content+'"';
+		if(data.type){
+			return '" + require("html-webpack-loader/plugins")(require(' + JSON.stringify('./'+data.src)+ ')) + "';
+		}else return '" + require(' + JSON.stringify('./'+data)+ ') + "';
+
+	})+ ";";
 }
 
-module.exports = _html_Style_loader;
+module.exports = _html_webpack_loader;
 
 
 
